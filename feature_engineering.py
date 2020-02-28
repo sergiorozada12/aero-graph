024de3c286e26565bb490f81e6333fd9a061b06e
Victor Manuel Tenorio <victor.030995@gmail.com>
2020-02-28 17:42:01 +0100

    Added interpolation and clustering

--- feature_engineering.py
+++ feature_engineering.py
@@ -21,3 +21,40 @@ df_od_med_delays = pd.DataFrame(avg_delays_od_pairs, columns=od_pairs)
 
 avg_delays_nodes_dep = df_nodes['MEDIAN_NODE_DEP_DELAY'].values.reshape(-1, nodes.shape[0]).astype(np.float32)
 avg_delays_nodes_arr = df_nodes['MEDIAN_NODE_ARR_DELAY'].values.reshape(-1, nodes.shape[0]).astype(np.float32)
+
+cols_dep = [n + '_DEPAR' for n in nodes]
+cols_arr = [n + '_ARRIV' for n in nodes]
+
+df_node_med_delays = pd.concat([pd.DataFrame(avg_delays_nodes_dep, columns=cols_dep),
+                                pd.DataFrame(avg_delays_nodes_arr, columns=cols_arr)], axis=1)
+
+df_med_delays = pd.concat([df_od_med_delays, df_node_med_delays], axis=1)
+
+assert len(df_od_med_delays) ==\
+       len(df_node_med_delays) ==\
+       len(df_med_delays) ==\
+       dates.shape[0]*hours.shape[0],\
+       "Dataframes doesn't have the correct length"
+
+
+######  INTERPOLATION   ######
+LIMIT_INT = 6
+NO_INT_HOUR = 4
+
+df_med_delays_int = df_med_delays.interpolate(limit=LIMIT_INT)
+df_med_delays_int.fillna(0, inplace=True)
+
+
+######  CLUSTERING   ######
+n_clust = 6
+random_state = 42
+
+kmeans_h = KMeans(n_clusters=n_clust, random_state=random_state)
+kmeans_h.fit(df_med_delays_int.values)
+df_med_delays_int['HOUR_CLUSTER'] = kmeans_h.labels_
+
+kmeans_d = KMeans(n_clusters=n_clust, random_state=random_state)
+kmeans_d.fit(df_med_delays_int.values.reshape(dates.shape[0], -1))
+df_med_delays_int['DAY_CLUSTER'] = np.repeat(kmeans_d.labels_, hours.shape[0])
+
+df_med_delays_int['DAY_CLUSTER-1'] = df_med_delays_int['DAY_CLUSTER'].shift(-hours.shape[0], fill_value=1)
