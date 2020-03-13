6a50e4578c6a3ea1982e8abdf52438d1e9e8d6ec
Victor Manuel Tenorio <victor.030995@gmail.com>
2020-03-13 19:20:46 +0100

    Including delay types interpolation

--- feature_engineering.py
+++ feature_engineering.py
@@ -5,8 +5,10 @@ import utils as u
 DATA_PATH = "C:\\Users\\victor\\Documents\\Aero_TFG\\data\\"
 OUT_PATH = "C:\\Users\\victor\\Documents\\Aero_TFG\\features\\"
 
+DELAY_TYPES = ['DELAY', 'CARRIER_DELAY', 'LATE_AIRCRAFT_DELAY', 'NAS_DELAY', 'SECURITY_DELAY', 'WEATHER_DELAY']
+
 df = pd.read_csv(DATA_PATH + 'dataset_od_pairs.csv', sep='|')
-df_casted = u.cast_df_raw_columns(df).drop(columns=['DEP_DELAY', 'ARR_DELAY'])
+df_casted = u.cast_df_raw_columns(df)
 
 print("Read OD Pairs data with shape " + str(df.shape))
 
@@ -23,6 +25,9 @@ df_casted.drop(columns=['FL_DATE'], inplace=True)
 
 df_avg_delays = u.get_features_df(df_casted, df_nodes, od_pairs, nodes)
 
+df_d_types = u.get_delay_types_df(df_casted, df_nodes, DELAY_TYPES, od_pairs, nodes)
+print(df_d_types.shape)
+
 assert len(df_avg_delays) ==\
        dates.shape[0]*hours.shape[0],\
        "Dataframes doesn't have the correct length"
@@ -37,6 +42,9 @@ print("Interpolation - ", end="")
 
 df_avg_delays = df_avg_delays.interpolate(limit=LIMIT_INT)
 df_avg_delays.fillna(0, inplace=True)
+
+df_d_types = df_d_types.interpolate(limit=LIMIT_INT)
+df_d_types.fillna(0, inplace=True)
 
 print("DONE")
 
@@ -55,8 +63,6 @@ print("DONE")
 
 print("----------------------------------------")
 print("Obtaining neighbour information")
-
-DELAY_TYPES = ['DELAY', 'CARRIER_DELAY', 'LATE_AIRCRAFT_DELAY', 'NAS_DELAY', 'SECURITY_DELAY', 'WEATHER_DELAY']
 
 # Load edges
 edges_nodes = np.load(DATA_PATH + "graph/edges_nodes.npy", allow_pickle=True)
@@ -83,5 +89,6 @@ print("----------------------------------------")
 print("Saving the data")
 
 df_avg_delays.to_csv(OUT_PATH + 'df_avg_delays.csv', sep='|', index=False, index_label=False)
+df_d_types.to_csv(OUT_PATH + 'df_d_types.csv', sep='|', index=False, index_label=False)
 df_casted.to_csv(OUT_PATH + 'incoming_delays_ods.csv', sep='|', index=False, index_label=False)
 df_nodes.to_csv(OUT_PATH + 'incoming_delays_nodes.csv', sep='|', index=False, index_label=False)
