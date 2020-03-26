8f3c0cb278c69eb591093c4bca977b3f33a10c51
Victor Manuel Tenorio <victor.030995@gmail.com>
2020-03-26 22:17:06 +0100

    Modified run_models script

--- runScripts/run_models.sh
+++ runScripts/run_models.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-cd /home/server/Aero/aero-graph/models
+basepath='/home/server/Aero'
+cd $basepath/aero-graph/models
 
 d=`date +%Y%m%d-%H%M`
 
-mkdir ../output/$d
+mkdir $basepath/output/$d
 
-python3 model_mlp.py 2>&1 | tee ../output/$d/model_mlp.out
-python3 model_gnn.py 2>&1 | tee ../output/$d/model_gnn.out
-python3 model_rf.py 2>&1 | tee ../output/$d/model_rf.out
-python3 model_dt.py 2>&1 | tee ../output/$d/model_dt.out
-python3 model_gbt.py 2>&1 | tee ../output/$d/model_gbt.out
-python3 model_knn.py 2>&1 | tee ../output/$d/model_knn.out
-python3 model_lr.py 2>&1 | tee ../output/$d/model_lr.out
-python3 model_mlp_sergio.py 2>&1 | tee ../output/$d/model_mlp_sergio.out
+python3 model_rf.py 2>&1 | tee $basepath/output/$d/model_rf.out
+python3 model_dt.py 2>&1 | tee $basepath/output/$d/model_dt.out
+python3 model_gbt.py 2>&1 | tee $basepath/output/$d/model_gbt.out
+python3 model_knn.py 2>&1 | tee $basepath/output/$d/model_knn.out
+python3 model_lr.py 2>&1 | tee $basepath/output/$d/model_lr.out
+python3 model_mlp.py 2>&1 | tee $basepath/output/$d/model_mlp.out
+python3 model_gnn.py 2>&1 | tee $basepath/output/$d/model_gnn.out
+python3 model_mlp_sergio.py 2>&1 | tee $basepath/output/$d/model_mlp_sergio.out
